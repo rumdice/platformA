96cff643f656cd839fb22dadabb42cf32d1d429f
rumdice <rumdice@gmail.com>
2026-04-04 16:20:54 +0900

    Redis 구성 버그 수정 완료.

--- Redis/run_redis_cluster.bat
+++ Redis/run_redis_cluster.bat
@@ -9,31 +9,6 @@ docker-compose down -v
 echo [2/3] Redis 노드 6개 실행 중...
 docker-compose up -d
 
-echo 컨테이너가 안정화될 때까지 10초간 대기합니다...
-timeout /t 10 /nobreak > nul
 
-echo [3/3] Redis 클러스터 구성 시작 (Hostname 방식)...
-:: host.docker.internal을 사용하여 클러스터를 묶습니다.
-docker exec -it redis-1 redis-cli --cluster create ^
-  host.docker.internal:6371 ^
-  host.docker.internal:6372 ^
-  host.docker.internal:6373 ^
-  host.docker.internal:6374 ^
-  host.docker.internal:6375 ^
-  host.docker.internal:6376 ^
-  --cluster-replicas 1 --cluster-yes
-
-echo.
-echo ==========================================
-echo Redis 클러스터 구성이 완료되었습니다!
-echo 상태 확인: docker exec -it redis-1 redis-cli cluster nodes
-echo ==========================================
-docker exec -it redis-1 redis-cli cluster nodes
-
-
-echo.
-echo ==========================================
-echo 로컬 환경에서 redis 연결시 hosts 파일 관리자 권한으로 수정 필요.
-echo 127.0.0.1 host.docker.internal 추가 및 기존 docker desktop 옵션 주석처리
-echo ==========================================
-pause
+echo [3/3] Cluster 구성을 확인한다.
+docker exec -it redis-master-1 redis-cli -p 6371 cluster nodes
