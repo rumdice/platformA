58948e2b28aaf5ed41b57a13f5718e6cbfbe91a4
rumdice <rumdice@gmail.com>
2026-04-06 10:07:02 +0900

    차후 테스트 시나리오 준비.

--- PlatformA/PlatformA.Game.DummyClient/Program.cs
+++ PlatformA/PlatformA.Game.DummyClient/Program.cs
@@ -13,13 +13,15 @@ namespace PlatformA.Game.DummyClient
             {
                 Console.Clear();
                 Console.WriteLine("==================================================");
-                Console.WriteLine("    🚀 PlatformA Integrated Simulator");
+                Console.WriteLine("    🚀 Dummy Client Integrated Simulator");
                 Console.WriteLine("==================================================");
-                Console.WriteLine(" 1. 게임 서버 직접 접속 (Auth + TCP)");
-                Console.WriteLine(" 2. Auth 인증, 대기열, 인게임 서버 로그인. 200유저 부하 테스트");
+                Console.WriteLine(" 1. 게임 서버 직접 접속 테스트 (C# TCP Socket)");
+                Console.WriteLine(" 2. [시나리오 2] Front Page Util.API 테스트.");
                 Console.WriteLine(" 3. [시나리오 3] 한명의 유저가 로그인 후 매칭 신청. 2개 실행.");
                 Console.WriteLine(" 4. [시나리오 4] 1000명 로그인 + 대기열 통과 부하 테스트");
-                Console.WriteLine(" 5. 종료");
+                Console.WriteLine(" 5. [시나리오 5] 1000명 로그인 + 대기열 통과 부하 테스트 + 매칭 시스템 부하 테스트 (준비중)");
+                Console.WriteLine(" 6. [시나리오 6] 매칭 시스템 레이팅별 매칭 테스트 (준비중)");
+                Console.WriteLine(" 0. 종료");
                 Console.WriteLine("==================================================");
                 Console.Write("원하는 테스트 모드의 번호를 입력하세요: ");
 
@@ -28,10 +30,10 @@ namespace PlatformA.Game.DummyClient
                 switch (input)
                 {
                     case "1":
-                        await GameClientScenario.RunAsync();
+                        //await GameClientScenario.RunAsync(); // TODO: 코드 수정
                         break;
                     case "2":
-                        await TicketingScenario.RunAsync();
+                        //await TicketingScenario.RunAsync(); // TODO: 코드 수정
                         break;
                     case "3":
                         await MatchingScenario.RunAsync();
@@ -40,7 +42,15 @@ namespace PlatformA.Game.DummyClient
                         await LoginWaitScenario_1.RunAsync();
                         break;
                     case "5":
-                        return; // 프로그램 종료
+                        Console.WriteLine("\n[경고] 준비중인 기능입니다.");
+                        await Task.Delay(500);
+                        break;
+                    case "6":
+                        Console.WriteLine("\n[경고] 준비중인 기능입니다.");
+                        await Task.Delay(500);
+                        break;
+                    case "0":
+                        return;
                     default:
                         Console.WriteLine("\n[경고] 잘못된 입력입니다.");
                         await Task.Delay(1000);
