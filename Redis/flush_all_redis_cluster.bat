d114aac1489f59cb4ff7b7bebf7509e1a2a9669b
rumdice <rumdice@gmail.com>
2026-04-13 08:30:15 +0900

    redis cluster 정리 스크립트 수정.

--- Redis/flush_all_redis_cluster.bat
+++ Redis/flush_all_redis_cluster.bat
@@ -1,3 +1,5 @@
-foreach ($port in 6371..6373) { docker exec -it redis-master-1 redis-cli -p $port FLUSHALL }
-
+@echo off
+for /L %%p in (6371, 1, 6373) do (
+    docker exec -it redis-master-1 redis-cli -p %%p FLUSHALL
+)
 pause
